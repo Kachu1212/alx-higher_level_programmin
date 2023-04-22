ca61d235f0cc2def97a7f7954509f324cf3582eb
Nwosu Chukwuemeka <cknwosu@gmail.com>
2023-04-21 17:54:56 -0700

    Python Circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Defines a rectangle class."""
 from models.base import Base
 
@@ -155,3 +154,4 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+
