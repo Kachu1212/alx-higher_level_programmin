bff88193a29df5496dc7589d246f199e59687876
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-07-03 12:38:46 +0100

    Updated 4-hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,5 @@ import requests
 if __name__ == "__main__":
     hop = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(hop.text.__class__))
     print("\t- type: {}".format(type(hop.text)))
     print("\t- content: {}".format(hop.text))
