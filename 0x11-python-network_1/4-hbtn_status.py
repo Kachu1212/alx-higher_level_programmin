41de37ed844585c56cefa2f341a6d40a5932d5f1
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-07-03 14:46:05 +0100

    Updated 4-hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    hop = requests.get("https://intranet.hbtn.io/status")
+    hop = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(hop.text)))
     print("\t- content: {}".format(hop.text))
