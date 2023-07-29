90f6288607c08fa261ec3dd43d05a4dd6088535e
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-07-29 17:14:22 +0100

    Updated 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,33 @@
 #!/usr/bin/node
 
-const request = require('request');
-const url = process.argv[2];
+const fetch = require('node-fetch');
 
-request.get(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
+function computeCompletedTasks(apiUrl) {
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    }
-  });
-  console.log(tasksCompleted);
-});
+      return response.json();
+    })
+    .then((data) => {
+      const userCompletedTasks = {};
+
+      data.forEach((task) => {
+        if (task.completed) {
+          const userId = task.userId;
+          userCompletedTasks[userId] = (userCompletedTasks[userId] || 0) + 1;
+        }
+      });
+
+      for (const userId in userCompletedTasks) {
+        console.log(`User ID ${userId}: ${userCompletedTasks[userId]} completed tasks`);
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+computeCompletedTasks(apiUrl);
